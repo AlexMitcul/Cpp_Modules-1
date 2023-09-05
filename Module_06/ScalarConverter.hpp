c2456a0931fab142b705eac768e5497c6d8700f3
Alexandru Mitcul <49638972+AlexMitcul@users.noreply.github.com>
2023-09-05 17:05:31 +0100

    We need to discuss that :)

--- Module_06/ScalarConverter.hpp
+++ Module_06/ScalarConverter.hpp
@@ -15,7 +15,16 @@
 
 # include <string>
 
-class ScalarConverter { //static class > only static members
+/*
+In C++, there is no concept of a "static class" in the same way that someother
+programming languages have a static class concept, like in C# or Java.
+In C++, classes can have static members,
+but the class itself is not declared as static.
+Question:
+Do we need to implement this class in canonical form?
+If answer if yes, can I set all methods as private?
+*/
+class ScalarConverter {
 	public:
 		static void	convert(const std::string& str);
 };
